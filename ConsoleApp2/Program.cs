251c0aeac28c3241c0b69b873ebed2e03701539b
tomas <tomas@DESKTOP-MV6VH7K>
2022-05-18 11:47:16 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Venligst skriv in passordlengde.");
+            string passwordLength = Console.ReadLine();
+
+            string pattren = "lLssdd";
+            var a = Validate(passwordLength);
+        }
+        static bool Validate(string passwordLength)
+        {
+            foreach (var C in passwordLength)
+            {
+                bool Pass = char.IsDigit(C);
+                if (Pass == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
